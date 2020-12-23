6b4ca6518b5b487e4d805e3d5fd06633d64184cd
James Peralta <jamesperalta35@gmail.com>
2020-12-23 09:39:07 -0700

    Added some JQuery to make the frontpage handle the input forms

--- NBATopPlayers/form_logic.js
+++ NBATopPlayers/form_logic.js
@@ -1,7 +1,39 @@
-
+$('input:checkbox').change(function() {
+    // this will contain a reference to the checkbox
+    if (this.checked) {
+        let myLabel = $("label[for='" + $(this).attr('id') + "']");
+        myLabel.css({
+            color: "green"
+        });
+    } else {
+        let myLabel = $("label[for='" + $(this).attr('id') + "']");
+        myLabel.css({
+            color: "white"
+        });
+    }
+});
 
 $('.slider').on('input', function () {
-    console.log($(this).data("yooo"));
     let name = $(this).data("val");
     $("#" + name).text($(this).val());
 });
+
+$(document).keypress(
+    function(event){
+        if (event.which == '13') {
+            event.preventDefault();
+        }
+    });
+
+$("#submitButton").click(function (){
+        alert("STFU");
+    }
+);
+
+$("#submitButton").hover(function () {
+    $(this).css({
+        backgroundColor: "blue"
+    })},function (){
+    $(this).css({
+        backgroundColor: "green"
+    })});
