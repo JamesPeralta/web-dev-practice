dbf1ce467d724ddaf40c66b6d7c1869a094d2714
James Peralta <jamesperalta35@gmail.com>
2020-12-25 22:13:55 -0700

    Drop down modals working

--- PelotonNavBar/main.js
+++ PelotonNavBar/main.js
@@ -1,13 +1,16 @@
-$("#products li").click(function () {
+$(".products li").click(function () {
     let currPress = $(this).find('.modal');
-
     let otherModals = $(".modal")
     for (const element of otherModals) {
-        let otherPress = $("#products").find('#' + element.id);
+        let otherPress = $(".products").find('#' + element.id);
         if (currPress.attr('id') !== otherPress.attr('id') && otherPress.is(':visible')){
             otherPress.toggle();
         }
     }
 
     currPress.toggle();
+});
+
+$(".modal").click(function (e) {
+    e.stopPropagation();
 });
