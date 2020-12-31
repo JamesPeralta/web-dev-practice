c5fdbb5ba02db1c56c8c938399727f1c3876e655
James Peralta <jamesperalta35@gmail.com>
2020-12-31 12:05:32 -0700

    At Lifting State Up, end recap

--- react-tut/src/index.js
+++ react-tut/src/index.js
@@ -34,12 +34,170 @@ class Clock extends React.Component {
     }
 }
 
+function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+}
+
+function NumberList(props) {
+    const numbers = props.numbers !== undefined ? props.numbers : [];
+    const listItems = numbers.map((number) => (
+        <li key={number.toString()}>
+            {number}
+        </li>)
+    );
+    return (
+      <ul>{listItems}</ul>
+    );
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    const userGreeting = (
+        <div>
+            <UserGreeting />
+            <h1>Hello User</h1>
+            <Clock/>
+            <NumberList numbers={[1,10,20,100]}/>
+        </div>
+    );
+    const guestGreeting = (
+        <div>
+            <GuestGreeting/>
+            <h1>Hello Guest</h1>
+        </div>
+    );
+
+    return (isLoggedIn ? userGreeting: guestGreeting);
+}
+
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this); // Gives access to this in there
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} name="name" onChange={this.handleChange}/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        );
+    }
+}
+
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.props.onTemperatureChange(e.target.value);
+    }
+
+    render() {
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature} onChange={this.handleChange}/>
+                <BoilingVerdict celsius={parseFloat(temperature)}/>
+            </fieldset>
+        );
+    }
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.toCelsius = this.toCelsius.bind(this);
+        this.toFahrenheit = this.toFahrenheit.bind(this);
+        this.tryConvert = this.tryConvert.bind(this);
+        this.state = {temperature: '37', scale: 'c'};
+    }
+
+    handleCelsiusChange(temperature) {
+        this.setState({scale: 'c', temperature});
+    }
+
+    handleFahrenheitChange(temperature) {
+        this.setState({scale: 'f', temperature});
+    }
+
+    toCelsius(fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    toFahrenheit(celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    tryConvert(temperature, convert) {
+        const input = parseFloat(temperature);
+        if (Number.isNaN(input)) {
+            return '';
+        }
+        const output = convert(input);
+        const rounded = Math.round(output * 1000) / 1000;
+        return rounded.toString();
+    }
+
+    render() {
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? this.tryConvert(temperature, this.toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? this.tryConvert(temperature, this.toFahrenheit) : temperature;
+        return (
+            <div>
+                <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelsiusChange}/>
+                <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange}/>
+                <BoilingVerdict celsius={parseFloat(celsius)}/>
+            </div>
+        );
+    }
+}
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The ware would boil.</p>;
+    }
+    else {
+        return <p>The water would not boil.</p>;
+    }
+}
+
 function App() {
     return (
         <div>
-            <Clock/>
-            <Clock/>
-            <Clock/>
+            <Calculator/>
         </div>
     );
 }
