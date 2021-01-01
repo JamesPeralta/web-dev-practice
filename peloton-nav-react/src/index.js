0cab778232c82feeb16e34b6df716cff41a00f20
James Peralta <jamesperalta35@gmail.com>
2020-12-31 22:46:54 -0700

    Nav bar structure going

--- peloton-nav-react/src/index.js
+++ peloton-nav-react/src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div>
+        <PelotonNav/>
+    </div>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function PelotonNav(props) {
+    return (
+        <div className="peloton-nav">
+            <Logo/>
+            <ProductList/>
+            <OtherItems/>
+        </div>
+    );
+}
+
+function Logo(props) {
+    return (
+        <div>
+            <img src="./peloton-logo.svg" alt="peloton"/>
+        </div>
+    );
+}
+
+function ProductList(props) {
+    return (
+        <ul className="products-list">
+            <li>Bikes</li>
+            <li>Tread</li>
+            <li>App</li>
+            <li>Classes</li>
+        </ul>
+    );
+}
+
+function OtherItems(props) {
+    return (
+        <ul className="other-items">
+            <li><img src="./location.svg" alt="#"/></li>
+            <li><img src="./user.svg" alt="#"/></li>
+            <li><img src="./shopping-cart.svg" alt="#"/></li>
+        </ul>
+    );
+}
+
