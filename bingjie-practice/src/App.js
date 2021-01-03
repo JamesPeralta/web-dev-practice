5f2d1fe7bdae7f6cd0b2107e3b8422841c16578d
James Peralta <jamesperalta35@gmail.com>
2021-01-02 20:20:54 -0700

    Convert Bingjies one to use hooks

--- bingjie-practice/src/App.js
+++ bingjie-practice/src/App.js
@@ -1,56 +1,44 @@
 import './App.css';
-import React from "react";
-import axios from "axios";
+import React, {useState} from "react";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0,
-            users: []
-        }
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+function App() {
+    const [count, setCount] = useState(0);
+    const [users, setUsers] = useState([]);
 
-    handleClick() {
-        axios.get(`https://randomuser.me/api?page=${this.state.count}`)
-            .then(response => {
-                this.setState(prevState => {
-                    let newUsers = [...prevState.users, {
-                        name: response.data.results[0].name.first,
-                        imgSrc: response.data.results[0].picture.thumbnail
-                    }];
+    const handleClick = () => {
+        fetch(`https://randomuser.me/api?page=${count}`)
+            .then(response => response.json())
+            .then(responseJson => {
+                setUsers(prevUsers => (
+                    [...prevUsers, {
+                        name: responseJson.results[0].name.first,
+                        imgSrc: responseJson.results[0].picture.thumbnail
+                    }]
+                ))
 
-                    return {
-                        users: newUsers,
-                        count: prevState.count + 1
-                    }
-                })
+                setCount(prevCount => prevCount + 1);
             })
             .catch(error => {
                 console.log("Error")
             })
     }
 
-    userProfile = (user, index) => (
-        <div key={index}>
-            <h1>{user.name}</h1>
-            <img src={user.imgSrc} alt="hello"/>
+    const userProfile = (user, index) => (
+            <div key={index}>
+                <h1>{user.name}</h1>
+                <img src={user.imgSrc} alt="hello"/>
+            </div>
+    )
+
+    return (
+        <div>
+            <h1>Rendered count: {count}</h1>
+            <button onClick={handleClick}>Click Me</button>
+            {users.map(userProfile)}
         </div>
     );
-
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.handleClick}>Click Me</button>
-                {this.state.users.map(this.userProfile)}
-            </div>
-        );
-    };
 }
-
 
 
 export default App;
